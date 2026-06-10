fdd96eae6524cc915b265e710390e22bac03c71b
Anandhu <anandhuma468@gmail.com>
2026-06-10 13:20:05 +0530

    setup homepage routing

--- home/views.py
+++ home/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    return HttpResponse('Home Page')
+
+def about(request):
+    return HttpResponse('About Page')
+
+def booking(request):
+    return HttpResponse('Booking Page')
+
+def doctors(request):
+    return HttpResponse('Doctors Page')
+
+def contact(request):
+    return HttpResponse('Contact Page')
