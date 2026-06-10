614cec431e826ad4ee0f40d696d95d26f375ceae
Anandhu <anandhuma468@gmail.com>
2026-06-10 14:59:14 +0530

    Update settings and home views

--- home/views.py
+++ home/views.py
@@ -2,16 +2,20 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('Home Page')
+    return render(request, 'index.html')   
 
 def about(request):
-    return HttpResponse('About Page')
+    return render(request, 'about.html')
 
 def booking(request):
-    return HttpResponse('Booking Page')
+    return render(request, 'booking.html')
 
 def doctors(request):
-    return HttpResponse('Doctors Page')
+    return render(request, 'doctors.html')
 
 def contact(request):
-    return HttpResponse('Contact Page')
+    return render(request, 'contact.html')
+
+def department(request):
+    return render(request, 'department.html')
+
