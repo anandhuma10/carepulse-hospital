3d37b457980e48f735f21d32bce44ee24d3cbe6d
Anandhu <anandhuma468@gmail.com>
2026-06-10 16:01:31 +0530

    Fix server crash by adding placeholder views and configuring URLs for home feature

--- home/urls.py
+++ home/urls.py
@@ -3,8 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('booking/', views.booking, name='booking'),
-    path('doctors/', views.doctors, name='doctors'),
-    path('contact/', views.contact, name='contact'),
+
 ]
