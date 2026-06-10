a9257b8e11a3df8f3ab39d95ddd56feb33d87223
Anandhu <anandhuma468@gmail.com>
2026-06-10 20:41:42 +0530

    Fix home views after merge

--- home/views.py
+++ home/views.py
@@ -1,13 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
-<<<<<<< HEAD
-def home(request):
-    return render(request, 'index.html')
-=======
+
 def index(request):
     return render(request, 'index.html')   
 
 def about(request):
     return render(request, 'about.html')
->>>>>>> feature/about
