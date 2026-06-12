32cc4e1cc7c607e22f5917f1b9a3716aa40f9fe1
Anandhu <anandhuma468@gmail.com>
2026-06-12 15:47:09 +0530

    department page working ongoing

--- carepulse_hospital/settings.py
+++ carepulse_hospital/settings.py
@@ -124,3 +124,5 @@ STATICFILES_DIRS = [
 
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
--- home/views.py
+++ home/views.py
@@ -2,21 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Department
 
-def department_list(request):
-    departments = Department.objects.all()
-    # ⚠️ Double check that 'department.html' matches your exact file name
-    return render(request, 'department.html', {'departments': departments})
+
 
 # Create your views here.
+
+def department(request):
+    departments = Department.objects.all()
+    return render(request, 'department.html', {
+        'departments': departments
+    })
 
 def index(request):
     return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
-
-def department(request):
-    return render(request, 'department.html')
 
 def doctors(request):
     return render(request, 'doctors.html')
