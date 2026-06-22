be54f94e8f37f946fa9f25747f9486c01c5b0176
Anandhu <anandhuma468@gmail.com>
2026-06-22 14:54:04 +0530

    Fix project name module pathway in admin script

--- create_admin.py
+++ create_admin.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings') # Replace with your project folder name
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carepulse_hospital.settings') # Replace with your project folder name
 django.setup()
 
 from django.contrib.auth import get_user_model
@@ -10,7 +10,7 @@ User = get_user_model()
 # Change these values to your preferred login info
 username = 'admin'
 email = 'admin@carepulse.com'
-password = 'YourSecurePassword123!' 
+password = 'carepulse123' 
 
 if not User.objects.filter(username=username).exists():
     print("Creating superuser...")
