3ad3621b0e82afed3cbd447e93e4f83db433e9d9
Anandhu <anandhuma468@gmail.com>
2026-08-20 15:36:38 +0530

    Prevent double booking for doctors

--- home/serializers.py
+++ home/serializers.py
@@ -13,7 +13,32 @@ class DoctorSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 class AppointmentSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Appointment
         fields = "__all__"
-        read_only_fields = ('patient', 'created_at')
+        read_only_fields = ("patient", "created_at")
+
+    def validate(self, data):
+
+        doctor = data.get("doctor")
+        appointment_date = data.get("appointment_date")
+        time_slot = data.get("time_slot")
+
+        queryset = Appointment.objects.filter(
+            doctor=doctor,
+            appointment_date=appointment_date,
+            time_slot=time_slot
+        )
+
+        # If updating an existing appointment,
+        # exclude the appointment being updated.
+        if self.instance:
+            queryset = queryset.exclude(pk=self.instance.pk)
+
+        if queryset.exists():
+            raise serializers.ValidationError(
+                "This doctor is already booked for this date and time."
+            )
+
+        return data
